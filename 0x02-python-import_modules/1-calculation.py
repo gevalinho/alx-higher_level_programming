55231a96b3375adf3c90dfdc72fa36fe8e3ca7c2
gevalinho <gevalinho1@gmail.com>
2023-01-11 15:57:16 -0800

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 if __name__ == "__main__":
 
     """Print the sum, difference, multiple and quotient of 10 and 5."""
@@ -11,7 +12,6 @@ if __name__ == "__main__":
     a = 10
 
     b = 5
-
 
 
     print("{} + {} = {}".format(a, b, add(a, b)))
